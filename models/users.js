6bf871c30c8a5cc7182891428aa2915c6bbe3a58
Abdulloh Fahmi <abdullohfahmi45@gmail.com>
2022-01-27 21:26:00 +0700

    add users model

---
+++ models/users.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  created_at: {
+    type: Date,
+    default: Date.now,
+  },
+  updated_at: {
+    type: Date,
+    default: null,
+  },
+});
+
+const User = model("User", userSchema);
+module.exports = User;
